1f440b01c4f7ea51496a38bcecdab4143560d163
felip <felip@DESKTOP-0VT61A2>
2023-03-26 19:11:52 -0300

    Adicionar arquivos de projeto.

---
+++ atividade 7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace IdadeVotarHabilitacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o ano de nascimento:");
+            int anoNascimento = int.Parse(Console.ReadLine());
+
+            // Calcula a idade da pessoa com base no ano atual
+            int idade = DateTime.Now.Year - anoNascimento;
+
+            Console.WriteLine($"Idade: {idade}");
+
+            // Verifica se a pessoa tem idade suficiente para votar e obter a CNH
+            if (idade >= 16)
+            {
+                Console.WriteLine("Você já pode votar!");
+            }
+            else
+            {
+                Console.WriteLine("Você ainda não pode votar!");
+            }
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("Você já pode obter a CNH!");
+            }
+            else
+            {
+                Console.WriteLine("Você ainda não pode obter a CNH!");
+            }
+        }
+    }
+}
+
